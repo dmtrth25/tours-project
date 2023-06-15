3fc0e3bd3b36d9a28d309f466f2fdf53e9aa43e0
dmtrth25 <vodolazhchenko25@gmail.com>
2023-06-15 11:42:17 +0300

    completed input fields for modal

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -8,6 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import useRegisterModal from "@/app/hooks/useRegisterModal"
 import Modal from "./Modal"
 import Heading from "../Heading"
+import Input from "../inputs/Input"
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
@@ -43,7 +44,32 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Test Header" subtitle="Create an account" />
+      <Heading title="Welcome!" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   )
 
