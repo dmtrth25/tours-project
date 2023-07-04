b13264c902ce90e959140433c4118875f63115a5
dmtrth25 <vodolazhchenko25@gmail.com>
2023-07-04 14:25:25 +0300

    step handlers for rent modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,16 +1,57 @@
 "use client"
 
+import { useMemo, useState } from "react"
+
 import Modal from "./Modal"
+
 import useRentModal from "@/app/hooks/useRentModal"
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal()
+
+  const [step, setStep] = useState(STEPS.CATEGORY)
+
+  const handleBack = () => {
+    setStep((step) => step - 1)
+  }
+
+  const handleNext = () => {
+    setStep((step) => step + 1)
+  }
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create"
+    }
+
+    return "Next"
+  }, [step])
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined
+    }
+
+    return "Back"
+  }, [step])
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : handleBack}
       title="Your home!"
     />
   )
