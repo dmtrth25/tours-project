2a8d8a28be0910d8e9b1c4bbe65a5ba5bca0f762
dmtrth25 <vodolazhchenko25@gmail.com>
2023-07-06 17:23:29 +0300

    select country with react-select

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,7 +1,58 @@
 "use client"
 
-const CountrySelect = () => {
-  return <div>CountrySelect</div>
+import useCountries from "@/app/hooks/useCountries"
+import { FC } from "react"
+import Select from "react-select"
+
+export type CountrySelectValue = {
+  flag: string
+  label: string
+  latlng: number[]
+  region: string
+  value: string
+}
+
+interface CountrySelectProps {
+  value?: CountrySelectValue
+  onChange: (value: CountrySelectValue) => void
+}
+
+const CountrySelect: FC<CountrySelectProps> = ({ value, onChange }) => {
+  const { getAll } = useCountries()
+  return (
+    <div>
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="flex flex-row items-center gap-3">
+            <div>{option.flag}</div>
+            <div>
+              {option.label},
+              <span className="text-neutral-500 ml-1">{option.region}</span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => "p-3 border-2",
+          input: () => "text-lg",
+          option: () => "text-lg",
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 6,
+          colors: {
+            ...theme.colors,
+            primary: "black",
+            primary25: "#a6f6cb",
+          },
+        })}
+      />
+    </div>
+  )
 }
 
 export default CountrySelect
