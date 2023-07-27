9e03d3b3f196a5c494d32321713b687afa27319e
dmtrth25 <vodolazhchenko25@gmail.com>
2023-07-27 13:47:16 +0300

    styles for ListingCard and created HeartButton

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,6 +6,9 @@ import { SafeUser } from "@/app/types"
 import { Listing, Reservation } from "@prisma/client"
 import useCountries from "@/app/hooks/useCountries"
 import { format } from "date-fns"
+import Image from "next/image"
+import HeartButton from "../HeartButton"
+import Button from "../Button"
 
 interface ListingCardProps {
   data: Listing
@@ -63,7 +66,60 @@ const ListingCard: FC<ListingCardProps> = ({
     return `${format(start, "PP")} - ${format(end, "PP")}`
   }, [reservation])
 
-  return <div>ListingCard</div>
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="
+        col-span-1 cursor-pointer group
+      "
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div
+          className="
+          w-full 
+          aspect-square
+          relative 
+          overflow-hidden 
+          rounded-xl
+        "
+        >
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default ListingCard
